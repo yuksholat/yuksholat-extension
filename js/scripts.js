4b6089f6722e1f5c02d493d1c66b07128abda45a
rizkix <rizki.fuad@gmail.com>
2018-07-24 08:26:36 +0700

    location + pouchdb + locate

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-var prayTimes = new PrayTimes('Kemenag')
-var times = prayTimes.getTimes(new Date(), [-6.2235414, 106.8398132], 7)
+var DEFAULT_LATITUDE = -5.7768256
+var DEFAULT_LONGITUDE = 106.397789
+var DEFAULT_TIMEZONE = 7
 
 var SHUBUH = document.querySelector(".fajr h3")
 var DZUHUR = document.querySelector(".dhuhr h3")
 var ASHAR = document.querySelector(".asr h3")
 var MAGHRIB = document.querySelector(".maghrib h3")
 var ISHA = document.querySelector(".isha h3")
+
+var cityText = document.querySelector("h4#city")
 
 var SHUBUH_WRAPPER = document.querySelector(".fajr")
 var DZUHUR_WRAPPER = document.querySelector(".dhuhr")
@@ -19,11 +22,10 @@ var ASHAR_TEXT = document.querySelector(".asr h2")
 var DZUHUR_TEXT = document.querySelector(".dhuhr h2")
 var ISHA_TEXT = document.querySelector(".isha h2")
 
-SHUBUH.innerHTML = times.fajr
-DZUHUR.innerHTML = times.dhuhr
-ASHAR.innerHTML = times.asr
-MAGHRIB.innerHTML = times.maghrib
-ISHA.innerHTML = times.isha
+
+var prayTimes = new PrayTimes('Kemenag')
+var times
+reloadPrayerTimes({latitude: DEFAULT_LATITUDE, longitude: DEFAULT_LONGITUDE}, DEFAULT_TIMEZONE)
 
 function fixtime(i) {
   if (i < 10) {
@@ -38,6 +40,9 @@ var todayDate = moment().format("YYYY-MM-DD"),
 tomorrowDate = moment().add(1, 'day').format("YYYY-MM-DD")
 
 function printtime() {
+  if (!times) {
+    setTimeout(printtime, 1000)
+  }
   var today = new Date();
   var h = fixtime(today.getHours())
   var m = fixtime(today.getMinutes())
@@ -90,3 +95,84 @@ function printtime() {
 
 printtime()
 
+var db = new PouchDB('yuksholat_db');
+
+function fetchLocation() {
+  navigator.permissions.query({name:'geolocation'}).then(function(result) {
+    navigator.geolocation.getCurrentPosition(saveLocation);
+    btnLocateMe.classList.add("hide");
+  })
+}
+
+function reloadPrayerTimes(location, tz) {
+  times = prayTimes.getTimes(new Date(), [location.latitude, location.longitude], tz)
+
+  SHUBUH.innerHTML = times.fajr
+  DZUHUR.innerHTML = times.dhuhr
+  ASHAR.innerHTML = times.asr
+  MAGHRIB.innerHTML = times.maghrib
+  ISHA.innerHTML = times.isha
+
+  cityText.innerHTML = location.city
+}
+
+function getLocation() {
+  db.get('location').then(function(location) {
+    if (!location) return fetchLocation()
+    reloadPrayerTimes(location, 7)
+  }).catch(function(e) {
+    fetchLocation()
+  })
+}
+
+function saveLocation(position) {
+  var location = {
+    _id: 'location',
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  }
+  
+
+  var data = {
+    key: 'AIzaSyA3UliJJ2Pni6qpL6BG2vlzpZWxip6zADg',
+    latlng: `${location.latitude}\,${location.longitude}`,
+    lang: 'id'
+  }
+
+  var url = 'https://maps.googleapis.com/maps/api/geocode/json?' + parseUrlEncoded(data)
+  var _loc
+  axios.get(url).then(function(resp) {
+    db.get('location').then(function(loc) {
+      loc.city = resp.data.results[2].formatted_address
+      _loc = loc
+      return db.put(loc)
+    }).then(function() {
+      reloadPrayerTimes(_loc, 7)
+    }).catch(function(e) {
+      reloadPrayerTimes(location, 7)
+    })
+  })
+
+
+}
+
+
+function parseUrlEncoded (data) {
+  var params = new URLSearchParams()
+  for (key in data) {
+    params.append(key, data[key])
+  }
+  return params
+}
+
+getLocation()
+
+var btnLocateMe = document.getElementById("btn-locate-me")
+
+document.getElementById("btn-location").addEventListener("click", function(){
+  btnLocateMe.classList.remove("hide");
+});
+
+btnLocateMe.addEventListener("click", function(){
+  fetchLocation()
+});
